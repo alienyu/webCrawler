ef7936f88cc9894cdc00832d4f01f82b879a7d13
alienyu <alienyu@foxmail.com>
2021-02-18 16:18:27 +0800

    update

--- chengduEDU/index.js
+++ chengduEDU/index.js
@@ -1,14 +1,16 @@
 const path = require("path");
 const getSchoolInfo = require("./getSchoolInfo");
 const genExcelFile = require("../common").genExcelFile;
-var defaultTotalPages = 1; //默认总页码；
+var defaultTotalPages = 1000; //默认总页码；
 var excelFilePath = path.join(__dirname, "成都学校信息.csv");
 
 async function generateSchoolInfoExcel(defaultTotalPages) {
+    var startTime = new Date().getTime();
     var schoolList = await getSchoolInfo(defaultTotalPages);
-    console.log('schoolList', schoolList)
-    console.log('length', schoolList.length)
+    console.log('总条数：', schoolList.length)
     genExcelFile(schoolList, excelFilePath)
+    var endTime = new Date().getTime();
+    console.log(`总耗时：${(endTime - startTime)/1000}秒`);
 }
 
-generateSchoolInfoExcel(defaultTotalPages, excelFilePath)
+generateSchoolInfoExcel(defaultTotalPages, excelFilePath);
